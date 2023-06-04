f045362e0ad9d855d50f2b7ffcec1e3761a939e7
Abigail Chávez <chavezlozanoabigail@gmail.com>
2023-06-03 23:03:51 -0500

    Calcular la mediana de un arreglo (no ordenado) con longtiud par o impar

---
+++ mediana.js
@@ -0,0 +1,33 @@
+const list1 = [4, 23, 8, 62, 12 ]
+const list2 = [15, 5, 8, 7, 62, 10]
+const list3 = [8, 47, 9, 10, 50, 1]
+
+function findAverage (arr) {
+    const addElements = arr.reduce((a, b) => a + b ) / arr.length
+    return addElements
+}
+
+
+function pairOrOddList (arr) {
+    if (arr.length % 2 === 0) {
+        return true //Si es par
+    } else {
+        return false //Si es impar
+    }
+}
+
+function calculateMedian (arr) {
+    if (pairOrOddList(arr) == true) {
+        const firstHalfArray = (arr.length / 2) -1
+        const secondHalfArray = arr.length / 2
+        return  findAverage([arr[firstHalfArray], arr[secondHalfArray]])
+
+    } else {
+        const halfOddArray =Math.floor(arr.length / 2)
+        return medianArray = arr[halfOddArray]
+    }
+}
+console.log( calculateMedian(list1));
+console.log( calculateMedian(list2));
+console.log( calculateMedian(list3));
+
